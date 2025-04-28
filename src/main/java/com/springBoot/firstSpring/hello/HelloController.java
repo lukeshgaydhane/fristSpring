bb4b64facb36070bbf7b8abb7df86aa27d029dd9
@lokeshgithu <lokeshgithub786@gmail.com>
2025-04-28 15:16:48 +0530

    (id- 002) Designing first "Hello World" api.

---
+++ src/main/java/com/springBoot/firstSpring/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.springBoot.firstSpring.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+//    http://localhost:8080/hello
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+}
