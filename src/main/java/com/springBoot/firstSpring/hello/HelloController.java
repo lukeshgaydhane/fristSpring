06125d91fac262a104d9cb56395e6fbc14a03c91
@lokeshgithu <lokeshgithub786@gmail.com>
2025-04-28 19:57:10 +0530

    (id- 003) Designing first "Hello World" POST API.

--- src/main/java/com/springBoot/firstSpring/hello/HelloController.java
+++ src/main/java/com/springBoot/firstSpring/hello/HelloController.java
@@ -1,6 +1,8 @@
 package com.springBoot.firstSpring.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+
+    //    http://localhost:8080/hello
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name){
+        return "Hello"+name+"!";
     }
 }
